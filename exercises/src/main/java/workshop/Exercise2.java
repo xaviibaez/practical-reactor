ff3f84a9c8a45d30cdcd30aa58c09e6da1a02b81
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-03-18 00:04:00 +0100

    Update Exercise2

--- exercises/src/main/java/workshop/Exercise2.java
+++ exercises/src/main/java/workshop/Exercise2.java
@@ -2,20 +2,33 @@ package workshop;
 
 import java.io.IOException;
 
+import static workshop.ReactiveSources.intNumbersFlux;
+import static workshop.ReactiveSources.userFlux;
+
 public class Exercise2 {
 
-    public static void main(String[] args) throws IOException {
+    public void run() throws IOException {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         // TODO: Write code here
+        printNumberFlux();
 
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
+        printUserFlux();
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+    public void printUserFlux() {
+        userFlux().subscribe(x -> System.out.println("Name: " + x.getFirstName() + " " + x.getLastName() + " ID: " + x.getId()));
+    }
+
+    public void printNumberFlux() {
+        intNumbersFlux().subscribe(System.out::println);
     }
 
 }
---
+++ exercises/src/test/java/workshop/Exercise2Test.java
@@ -0,0 +1,51 @@
+package workshop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Exercise2Test {
+
+    @Test
+    void it_should_print_all_numbers_in_reactive_sources_intNumbersFlux_stream() throws InterruptedException {
+        ByteArrayOutputStream outContent = getByteArrayOutputStream();
+
+        exercise2.printNumberFlux();
+
+        Thread.sleep(11000);
+
+        assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n", outContent.toString());
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    void it_should_print_all_users_in_reactive_sources_userFlux_stream() throws InterruptedException {
+        ByteArrayOutputStream outContent = getByteArrayOutputStream();
+
+        exercise2.printUserFlux();
+
+        Thread.sleep(7000);
+
+        assertEquals("Name: Lionel Messi ID: 1\nName: Cristiano Ronaldo ID: 2\nName: Diego Maradona ID: 2\nName: Zinedine Zidane ID: 4\nName: Jürgen Klinsmann ID: 5\nName: Gareth Bale ID: 6\n", outContent.toString());
+
+        System.setOut(System.out);
+    }
+
+    private ByteArrayOutputStream getByteArrayOutputStream() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        return outContent;
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(System.out);
+    }
+
+    Exercise2 exercise2 = new Exercise2();
+}
