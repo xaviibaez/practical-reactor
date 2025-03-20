0f815c0508fee8c9f4a6bf7093a7bba295ee80c2
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-03-20 23:25:18 +0100

    Update Exercise3

--- exercises/src/main/java/workshop/Exercise3.java
+++ exercises/src/main/java/workshop/Exercise3.java
@@ -1,20 +1,45 @@
 package workshop;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static workshop.ReactiveSources.intNumbersFlux;
 
 public class Exercise3 {
 
-    public static void main(String[] args) throws IOException {
-
+    public void run() throws IOException {
         // Use ReactiveSources.intNumbersFlux()
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
         // TODO: Write code here
 
+        getAllNumbers();
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+    public void getAllNumbers() {
+        //Option 1:
+        //List<Integer> l = new ArrayList<>();
+        //intNumbersFlux().doOnNext(l::add).subscribe();
+        //System.out.println("List: " + l);
+        //System.out.println("Size: " + l.size());
+
+        //Option 2:
+        //intNumbersFlux()
+        //        .collectList()
+        //        .subscribe(numbersList -> {
+        //            System.out.println("List: " + numbersList);
+        //            System.out.println("Size: " + numbersList.size());
+        //        });
+
+        //Option 3 blocking:
+        List<Integer> numbers = intNumbersFlux().toStream().toList();
+        System.out.println("List: " + numbers);
+        System.out.println("Size: " + numbers.size());
     }
 
 }
---
+++ exercises/src/test/java/workshop/Exercise3Test.java
@@ -0,0 +1,42 @@
+package workshop;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Exercise3Test {
+
+    @Test
+    void it_should_get_all_numbers_in_the_ReactiveSources_intNumbersFlux_stream_into_a_List_and_print_the_list_and_its_size() throws InterruptedException {
+        ByteArrayOutputStream outContent = getByteArrayOutputStream();
+
+        exercise3.getAllNumbers();
+
+        Thread.sleep(11000);
+
+        assertEquals("List: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]\nSize: 10\n", outContent.toString());
+    }
+
+    private ByteArrayOutputStream getByteArrayOutputStream() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        return outContent;
+    }
+
+    Exercise3 exercise3 = new Exercise3();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(System.out);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+}
