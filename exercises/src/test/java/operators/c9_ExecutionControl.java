5b9f3cfea1c4dda3d550dc7bc730b4076ee8f47c
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-07-28 00:19:25 +0200

    Refactor c9 ExecutionControl test cases for improved clarity and update threading logic

--- exercises/src/test/java/operators/c9_ExecutionControl.java
+++ exercises/src/test/java/operators/c9_ExecutionControl.java
@@ -2,7 +2,8 @@ package operators;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import reactor.blockhound.BlockHound;
 import reactor.core.Exceptions;
 import reactor.core.Scannable;
@@ -21,17 +22,17 @@ import java.util.stream.Collectors;
 /**
  * With multi-core architectures being a commodity nowadays, being able to easily parallelize work is important.
  * Reactor helps with that by providing many mechanisms to execute work in parallel.
- *
+ * <p>
  * Read first:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#schedulers
  * https://projectreactor.io/docs/core/release/reference/#advanced-parallelizing-parralelflux
  * https://projectreactor.io/docs/core/release/reference/#_the_publishon_method
  * https://projectreactor.io/docs/core/release/reference/#_the_subscribeon_method
  * https://projectreactor.io/docs/core/release/reference/#which.time
- *
+ * <p>
  * Useful documentation:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#which-operator
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
@@ -44,23 +45,46 @@ public class c9_ExecutionControl extends ExecutionControlBase {
      * You are working on smartphone app and this part of code should show user his notifications. Since there could be
      * multiple notifications, for better UX you want to slow down appearance between notifications by 1 second.
      * Pay attention to threading, compare what code prints out before and after solution. Explain why?
+     * <p>
+     * Before: Without delayElements, all notifications are emitted on the same thread as the invoker thread, and they appear immediately.
+     * After: With delayElements, each notification is delayed by 1 second, and the execution is moved to a different thread (not the invoker thread). This is because delayElements uses a Scheduler to manage the delay.
+     * <p>
+     * Before:
+     * New SMS message!
+     * -> On the same thread
+     * <p>
+     * After:
+     * New SMS message!
+     * -> Not on the same thread
+     * Missed call!
+     * -> Not on the same thread
+     * New email!
+     * -> Not on the same thread
+     * Update available!
+     * -> Not on the same thread
+     * New calendar event!
+     * -> Not on the same thread
      */
     @Test
     public void slow_down_there_buckaroo() {
         long threadId = Thread.currentThread().getId();
+        System.out.println("threadId = " + threadId);
         Flux<String> notifications = readNotifications()
                 .doOnNext(System.out::println)
                 //todo: change this line only
-                ;
+                .delayElements(Duration.ofSeconds(1));
 
         StepVerifier.create(notifications
-                                    .doOnNext(s -> assertThread(threadId)))
-                    .expectNextCount(5)
-                    .verifyComplete();
+                        .doOnNext(s -> assertThread(threadId)))
+                .expectNextCount(5)
+                .verifyComplete();
     }
 
     private void assertThread(long invokerThreadId) {
         long currentThread = Thread.currentThread().getId();
+        System.out.println("invokerThreadId = " + invokerThreadId);
+        System.out.println("currentThread = " + currentThread);
+
         if (currentThread != invokerThreadId) {
             System.out.println("-> Not on the same thread");
         } else {
@@ -79,17 +103,16 @@ public class c9_ExecutionControl extends ExecutionControlBase {
     public void ready_set_go() {
         //todo: feel free to change code as you need
         Flux<String> tasks = tasks()
-                .flatMap(Function.identity());
-        semaphore();
+                .concatMap(task -> task.delaySubscription(semaphore()));
 
         //don't change code below
         StepVerifier.create(tasks)
-                    .expectNext("1")
-                    .expectNoEvent(Duration.ofMillis(2000))
-                    .expectNext("2")
-                    .expectNoEvent(Duration.ofMillis(2000))
-                    .expectNext("3")
-                    .verifyComplete();
+                .expectNext("1")
+                .expectNoEvent(Duration.ofMillis(2000))
+                .expectNext("2")
+                .expectNoEvent(Duration.ofMillis(2000))
+                .expectNext("3")
+                .verifyComplete();
     }
 
     /**
@@ -97,37 +120,52 @@ public class c9_ExecutionControl extends ExecutionControlBase {
      * Answer:
      * - Which types of schedulers Reactor provides?
      * - What is their purpose?
+     * The purpose of the schedulers is to control the execution context of tasks,
+     * enabling efficient use of system resources and optimizing
+     * performance for different types of workloads
      * - What is their difference?
+     * <p>
+     * Schedulers.immediate(): Executes tasks on the current thread immediately.
+     * Schedulers.single(): Uses a single-threaded scheduler for tasks.
+     * Schedulers.elastic(): Creates a new thread for each task and reuses idle threads.
+     * Schedulers.boundedElastic(): Designed for blocking tasks, it limits the number of threads and reuses idle ones.
+     * Schedulers.parallel(): Optimized for parallelized, non-blocking tasks, using a fixed-size pool of threads.
+     * Schedulers.fromExecutor(): Allows custom executors to be used as schedulers.
+     * Schedulers.fromExecutorService(): Similar to fromExecutor, but uses an ExecutorService.
+     * Schedulers.onScheduleHook(): Adds hooks to intercept scheduling actions.
      */
     @Test
     public void non_blocking() {
         Mono<Void> task = Mono.fromRunnable(() -> {
-                                  Thread currentThread = Thread.currentThread();
-                                  assert NonBlocking.class.isAssignableFrom(Thread.currentThread().getClass());
-                                  System.out.println("Task executing on: " + currentThread.getName());
-                              })
-                              //todo: change this line only
-                              .then();
+                    Thread currentThread = Thread.currentThread();
+                    assert NonBlocking.class.isAssignableFrom(Thread.currentThread().getClass());
+                    System.out.println("Task executing on: " + currentThread.getName());
+                })
+                //todo: change this line only
+                .subscribeOn(Schedulers.parallel())
+                .then();
 
         StepVerifier.create(task)
-                    .verifyComplete();
+                .verifyComplete();
     }
 
     /**
      * Make task run on thread suited for long, blocking, parallelized work.
      * Answer:
      * - What BlockHound for?
+     * BlockHound is a tool to detect blocking calls from non-blocking threads.
+     * To use it, you need to "install"
      */
     @Test
     public void blocking() {
         BlockHound.install(); //don't change this line
 
         Mono<Void> task = Mono.fromRunnable(ExecutionControlBase::blockingCall)
-                              .subscribeOn(Schedulers.single())//todo: change this line only
-                              .then();
+                .subscribeOn(Schedulers.boundedElastic())//todo: change this line only
+                .then();
 
         StepVerifier.create(task)
-                    .verifyComplete();
+                .verifyComplete();
     }
 
     /**
@@ -136,15 +174,17 @@ public class c9_ExecutionControl extends ExecutionControlBase {
     @Test
     public void free_runners() {
         //todo: feel free to change code as you need
-        Mono<Void> task = Mono.fromRunnable(ExecutionControlBase::blockingCall);
+        Mono<Void> task = Mono.fromRunnable(ExecutionControlBase::blockingCall)
+                .subscribeOn(Schedulers.boundedElastic())
+                .then();
 
         Flux<Void> taskQueue = Flux.just(task, task, task)
-                                   .concatMap(Function.identity());
+                .flatMap(Function.identity(), 3);
 
         //don't change code below
         Duration duration = StepVerifier.create(taskQueue)
-                                        .expectComplete()
-                                        .verify();
+                .expectComplete()
+                .verify();
 
         Assertions.assertTrue(duration.getSeconds() <= 2, "Expected to complete in less than 2 seconds");
     }
@@ -156,15 +196,20 @@ public class c9_ExecutionControl extends ExecutionControlBase {
     public void sequential_free_runners() {
         //todo: feel free to change code as you need
         Flux<String> tasks = tasks()
-                .flatMap(Function.identity());
-        ;
+                .flatMapSequential(Function.identity());
+        /*
+         * Explanation:
+         * subscribeOn(Schedulers.boundedElastic()) it is not needed because you are not calling a blocking code.
+         * flatMapSequential(Function.identity()) is used to ensure that the order of the tasks is preserved
+         * while allowing them to run in parallel.
+         */
 
         //don't change code below
         Duration duration = StepVerifier.create(tasks)
-                                        .expectNext("1")
-                                        .expectNext("2")
-                                        .expectNext("3")
-                                        .verifyComplete();
+                .expectNext("1")
+                .expectNext("2")
+                .expectNext("3")
+                .verifyComplete();
 
         Assertions.assertTrue(duration.getSeconds() <= 1, "Expected to complete in less than 1 seconds");
     }
@@ -178,25 +223,28 @@ public class c9_ExecutionControl extends ExecutionControlBase {
     public void event_processor() {
         //todo: feel free to change code as you need
         Flux<String> eventStream = eventProcessor()
+                .parallel()
+                .runOn(Schedulers.parallel())
                 .filter(event -> event.metaData.length() > 0)
                 .doOnNext(event -> System.out.println("Mapping event: " + event.metaData))
                 .map(this::toJson)
-                .concatMap(n -> appendToStore(n).thenReturn(n));
+                .sequential()
+                .concatMap(n -> appendToStore(n).thenReturn(n), 3);
 
         //don't change code below
         StepVerifier.create(eventStream)
-                    .expectNextCount(250)
-                    .verifyComplete();
+                .expectNextCount(250)
+                .verifyComplete();
 
         List<String> steps = Scannable.from(eventStream)
-                                      .parents()
-                                      .map(Object::toString)
-                                      .collect(Collectors.toList());
+                .parents()
+                .map(Object::toString)
+                .collect(Collectors.toList());
 
         String last = Scannable.from(eventStream)
-                               .steps()
-                               .collect(Collectors.toCollection(LinkedList::new))
-                               .getLast();
+                .steps()
+                .collect(Collectors.toCollection(LinkedList::new))
+                .getLast();
 
         Assertions.assertEquals("concatMap", last);
         Assertions.assertTrue(steps.contains("ParallelMap"), "Map operator not executed in parallel");
