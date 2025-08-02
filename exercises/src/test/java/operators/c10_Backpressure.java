1cd814d99d1cda201efef8a9e13130fb7e554b1a
Xavi Ibáñez Martínez <javier.ibanez.martinez.tech@bbva.com>
2025-08-03 01:09:02 +0200

    Refactor c9 Backpressure test cases for improved clarity and update threading logic

--- exercises/src/test/java/operators/c10_Backpressure.java
+++ exercises/src/test/java/operators/c10_Backpressure.java
@@ -50,7 +50,7 @@ public class c10_Backpressure extends BackpressureBase {
         CopyOnWriteArrayList<Long> requests = new CopyOnWriteArrayList<>();
         Flux<String> messageStream = messageStream1()
                 //todo: change this line only
-                ;
+                .doOnRequest(requests::add);
 
         StepVerifier.create(messageStream, StepVerifierOptions.create().initialRequest(0))
                     .expectSubscription()
@@ -74,6 +74,8 @@ public class c10_Backpressure extends BackpressureBase {
         CopyOnWriteArrayList<Long> requests = new CopyOnWriteArrayList<>();
         Flux<String> messageStream = messageStream2()
                 //todo: do your changes here
+                .doOnRequest(requests::add)
+                .limitRate(1)
                 ;
 
         StepVerifier.create(messageStream, StepVerifierOptions.create().initialRequest(0))
@@ -95,9 +97,11 @@ public class c10_Backpressure extends BackpressureBase {
      */
     @Test
     public void uuid_generator() {
-        Flux<UUID> uuidGenerator = Flux.create(sink -> {
-            //todo: do your changes here
-        });
+        Flux<UUID> uuidGenerator = Flux.create(sink -> sink.onRequest(r -> {
+            for (int i = 0; i < r; i++) {
+                sink.next(UUID.randomUUID());
+            }
+        }));
 
         StepVerifier.create(uuidGenerator
                                     .doOnNext(System.out::println)
@@ -119,7 +123,7 @@ public class c10_Backpressure extends BackpressureBase {
     public void pressure_is_too_much() {
         Flux<String> messageStream = messageStream3()
                 //todo: change this line only
-                ;
+                .onBackpressureError();
 
         StepVerifier.create(messageStream, StepVerifierOptions.create()
                                                               .initialRequest(0))
@@ -140,6 +144,7 @@ public class c10_Backpressure extends BackpressureBase {
     public void u_wont_brake_me() {
         Flux<String> messageStream = messageStream4()
                 //todo: change this line only
+                .onBackpressureBuffer()
                 ;
 
         StepVerifier.create(messageStream, StepVerifierOptions.create()
@@ -176,12 +181,15 @@ public class c10_Backpressure extends BackpressureBase {
                     @Override
                     protected void hookOnSubscribe(Subscription subscription) {
                         sub.set(subscription);
+                        subscription.request(10);
                     }
 
                     @Override
                     protected void hookOnNext(String s) {
                         System.out.println(s);
-                        count.incrementAndGet();
+                        if(count.incrementAndGet() >= 10) {
+                            sub.get().cancel();
+                        }
                     }
                     //-----------------------------------------------------
                 });
